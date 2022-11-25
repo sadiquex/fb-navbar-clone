00e3b7af297a780e97de3b93f77bc76c28500179
sadiquex <abubakasaddik1@gmail.com>
2022-11-25 23:52:13 +0000

    added icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import react from "react";
+import "./index.css";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
+
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Navbar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+    </Navbar>
   );
 }
 
-function Navbar(props) {
-  <h1>the navbar</h1>;
+function Navbar(props) {}
+
+function NavItem(props) {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  );
 }
 
 export default App;
